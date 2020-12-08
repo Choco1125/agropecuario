bcd064380fe9672163a4425d9cbd5d96de30f23c
Choco1125 <sebascruz981125@gmail.com>
2020-12-08 16:09:39 -0500

    Correccion(Ventas: Gerente) Visualizacion de ventas por finca

--- sitio/Owner/ventas.php
+++ sitio/Owner/ventas.php
@@ -1,156 +1,156 @@
 <?php
-  include "main.php";
- ?>
+include "main.php";
+?>
 
 <link rel="stylesheet" href="../../css/datatables.min.css">
 
 </body>
 
 <style>
-  .contenedor{
+  .contenedor {
     overflow: auto;
     width: 100%;
   }
 
-i {
-  color: white;
-}
+  i {
+    color: white;
+  }
 
-th, td {
-  text-align: center;
-}
-
+  th,
+  td {
+    text-align: center;
+  }
 </style>
 
 <br><br>
 <div class="container contenedor ">
 
 
-<h3> Ventas -  
-  <?php 
+  <h3> Ventas -
+    <?php
     include "../../php/conection.php";
-    $query = "SELECT nombre FROM finca WHERE id = ".$_SESSION['finca'];
+    $query = "SELECT nombre FROM finca WHERE id = " . $_SESSION['finca'];
     $sql = mysqli_query($conection, $query);
     if ($sql) {
       echo mysqli_fetch_array($sql)['nombre'];
     }
-  ?> 
-</h3>
-<br>
+    ?>
+  </h3>
+  <br>
 
-<!-- Tabla de bodega -->
-<table class="table" id="tabla">
-  <thead>
-    <tr>
-      <th scope="col"> Nro. Remisión </th>
-      <th scope="col"> Fecha </th>
-      <th scope="col"> Nombre Lote </th>
-      <th scope="col"> Nombre Empleado </th>
-      <th scope="col"> Nombre Producto </th>
-      <th scope="col"> Cantidad </th>
-      <th scope="col"> Valor </th>
-      <th scope="col"> Acciones </th>
-    </tr>
-  </thead>
-  <tbody>
+  <!-- Tabla de bodega -->
+  <table class="table" id="tabla">
+    <thead>
+      <tr>
+        <th scope="col"> Nro. Remisión </th>
+        <th scope="col"> Fecha </th>
+        <th scope="col"> Nombre Lote </th>
+        <th scope="col"> Nombre Empleado </th>
+        <th scope="col"> Nombre Producto </th>
+        <th scope="col"> Cantidad </th>
+        <th scope="col"> Valor </th>
+        <th scope="col"> Acciones </th>
+      </tr>
+    </thead>
+    <tbody>
 
-<?php
+      <?php
 
-  //Imprimimos los registros de la base DB
-  $id_finca = $_SESSION['finca'];
-  include "../../php/conection.php";
-  $query = "SELECT id, n_remision, fecha, nombre_lote, nombre_empleado, nombre_producto, cantidad, valor FROM ventas";
-  $sql = mysqli_query($conection, $query);
-  if ($sql) {
-    while ($row = mysqli_fetch_array($sql)) {
-?>
-  <tr>
-    <td> <?php echo $row['n_remision'] ?></td>
-    <td> <?php echo $row['fecha'] ?></td>
-    <td> <?php echo $row['nombre_lote'] ?></td>
-    <td> <?php echo $row['nombre_empleado'] ?></td>
-    <td> <?php echo $row['nombre_producto'] ?></td>
-    <td> <?php echo $row['cantidad'] ?></td>
-    <td> <?php echo $row['valor'] ?></td>
-    <td> 
-      <a class="btn btn-danger" onclick="eliminar(<?php echo $row['id'] ?>);"> 
-        <i class="fas fa-trash-alt"> </i> 
-      </a>
-    </td>
-  </tr>
+      //Imprimimos los registros de la base DB
+      $id_finca = $_SESSION['finca'];
+      include "../../php/conection.php";
+      $query = "SELECT ventas.id, ventas.n_remision, ventas.fecha, lote.nombre AS nombre_lote, cliente.nombre AS nombre_empleado, producto.nombre AS nombre_producto, ventas.cantidad, ventas.valor FROM ventas INNER JOIN lote ON ventas.lote_id = ventas.lote_id INNER JOIN cliente ON ventas.cliente_id = cliente.id INNER JOIN producto ON ventas.producto_id = producto.id WHERE lote.id_finca = " . $id_finca;
+      $sql = mysqli_query($conection, $query);
+      if ($sql) {
+        while ($row = mysqli_fetch_array($sql)) {
+      ?>
+          <tr>
+            <td> <?php echo $row['n_remision'] ?></td>
+            <td> <?php echo $row['fecha'] ?></td>
+            <td> <?php echo $row['nombre_lote'] ?></td>
+            <td> <?php echo $row['nombre_empleado'] ?></td>
+            <td> <?php echo $row['nombre_producto'] ?></td>
+            <td> <?php echo $row['cantidad'] ?></td>
+            <td> <?php echo $row['valor'] ?></td>
+            <td>
+              <a class="btn btn-danger" onclick="eliminar(<?php echo $row['id'] ?>);">
+                <i class="fas fa-trash-alt"> </i>
+              </a>
+            </td>
+          </tr>
 
-<?php
-    }
-  }
+      <?php
+        }
+      }
 
 
- ?>
-  
-  </tbody>
+      ?>
+
+    </tbody>
   </table>
-<!-- Cierre de tabla del modal para ver bodega -->
+  <!-- Cierre de tabla del modal para ver bodega -->
 
 
-<?php
-include "end.php";
-?>
+  <?php
+  include "end.php";
+  ?>
 
-<script src="../../js/datatables.min.js"> </script>
-<script>
- $(document).ready(function() {
+  <script src="../../js/datatables.min.js"> </script>
+  <script>
+    $(document).ready(function() {
       $('#tabla').DataTable({
         language: {
-            search: "Buscar Registro",
-                paginate: {
-                  first:      "Primero",
-                  previous:   "Anterior",
-                  next:       "Siguiente",
-                  last:       "Último"
-                },
-                "lengthMenu":     "Mostar _MENU_ Por Página",
-                "zeroRecords": "No Se Encontraron Registros",
-                "info": "Página _PAGE_ de _PAGES_",
-                "infoEmpty": "No Se Encontraron Registros",
-                "infoFiltered": "(Se Filtraron _MAX_ Registros)",
-            }
-      })
-   });
-
-//Eliminar
-function eliminar(id){
-    swal({
-    title: "Advertencia",
-    text: "¿Seguro de querer eliminar este campo?",
-    icon: "warning",
-    buttons: true,
-   }).then((res)=>{
-    if(res){
-      $.ajax({
-        url: '../../php/Owner/ventas.php',
-        type: 'POST',
-        data: {
-          op: 'delete',
-          id: id
+          search: "Buscar Registro",
+          paginate: {
+            first: "Primero",
+            previous: "Anterior",
+            next: "Siguiente",
+            last: "Último"
+          },
+          "lengthMenu": "Mostar _MENU_ Por Página",
+          "zeroRecords": "No Se Encontraron Registros",
+          "info": "Página _PAGE_ de _PAGES_",
+          "infoEmpty": "No Se Encontraron Registros",
+          "infoFiltered": "(Se Filtraron _MAX_ Registros)",
         }
       })
-      .done(function(data) {
-        console.log(data);
-          if(data == 1){
-            swal({
-            title: "Buen trabajo",
-            text: "Se elimino correctamente el campo",
-            icon: "success",
-            button: true,
-          }).then((res)=>{
-            location.reload();
-          });
-          } else {
-            swal("¡Error!","No se logró eliminar", "error");
+    });
+
+    //Eliminar
+    function eliminar(id) {
+      swal({
+        title: "Advertencia",
+        text: "¿Seguro de querer eliminar este campo?",
+        icon: "warning",
+        buttons: true,
+      }).then((res) => {
+        if (res) {
+          $.ajax({
+              url: '../../php/Owner/ventas.php',
+              type: 'POST',
+              data: {
+                op: 'delete',
+                id: id
+              }
+            })
+            .done(function(data) {
+              console.log(data);
+              if (data == 1) {
+                swal({
+                  title: "Buen trabajo",
+                  text: "Se elimino correctamente el campo",
+                  icon: "success",
+                  button: true,
+                }).then((res) => {
+                  location.reload();
+                });
+              } else {
+                swal("¡Error!", "No se logró eliminar", "error");
+              }
+            });
         }
       });
     }
-  });
-}
-</script>
+  </script>
 
- </html>
+  </html>
