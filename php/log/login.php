cae4dda4f39b372d76c1019e72bf6177f213d8db
Choco1125 <sebascruz981125@gmail.com>
2020-12-09 14:06:39 -0500

    Correccion(Login: Comprador) Se activan las redirecciones para el rol

--- php/log/login.php
+++ php/log/login.php
@@ -1,62 +1,64 @@
-<?php 
-	if (isset($_POST['nombre'])) {
-		//obtenemos datos del post
-		$nombre = $_POST['nombre'];
-		$pass = md5($_POST['pass']);
+<?php
+if (isset($_POST['nombre'])) {
+	//obtenemos datos del post
+	$nombre = $_POST['nombre'];
+	$pass = md5($_POST['pass']);
 
 
-		//consultamos la BD
-		include "../conection.php";
-		$query = "SELECT * FROM usuarios WHERE nombre_usuario='$nombre' AND contrasena='$pass'";
+	//consultamos la BD
+	include "../conection.php";
+	$query = "SELECT * FROM usuarios WHERE nombre_usuario='$nombre' AND contrasena='$pass'";
 
-		$sql = mysqli_query($conection,$query);
+	$sql = mysqli_query($conection, $query);
 
-		if($sql){
-			$num = mysqli_num_rows($sql);
-			if($num > 0){
-				$datos = mysqli_fetch_array($sql);
-				session_start();
-				$_SESSION['login'] = true;
-				$_SESSION['id']    = $datos['id'];
-				$_SESSION['id_rol'] = $datos['id_rol'];
+	if ($sql) {
+		$num = mysqli_num_rows($sql);
+		if ($num > 0) {
+			$datos = mysqli_fetch_array($sql);
+			session_start();
+			$_SESSION['login'] = true;
+			$_SESSION['id']    = $datos['id'];
+			$_SESSION['id_rol'] = $datos['id_rol'];
 
 
-				//redireccionar a donde sea necesario
-				switch ($datos['id_rol']) {
-					case "1":
-						header("location: ../../sitio/");
-						$_SESSION['carpeta'] = 'Admin';
-						break;
-					case "2":
-						header("location: ../../sitio/");
-						$_SESSION['carpeta'] = 'Owner' ;
-						break;
-					case "3":
-						header("location: ../../sitio/");
-						$_SESSION['carpeta'] = 'Comprador';
-						break;
-					case "4":
-						header("location: ../../sitio/");
-						$_SESSION['carpeta'] = 'Vendedor';
-						break;					
-				}
-
-			} else {
-				header("location: ../../index.php");
+			//redireccionar a donde sea necesario
+			switch ($datos['id_rol']) {
+				case "1":
+					header("location: ../../sitio/");
+					$_SESSION['carpeta'] = 'Admin';
+					break;
+				case "2":
+					header("location: ../../sitio/");
+					$_SESSION['carpeta'] = 'Owner';
+					break;
+				case "3":
+					header("location: ../../sitio/");
+					$_SESSION['carpeta'] = 'Comprador';
+					break;
+				case "4":
+					header("location: ../../sitio/");
+					$_SESSION['carpeta'] = 'Vendedor';
+					break;
+				case "5":
+					header("location: ../../sitio/");
+					$_SESSION['carpeta'] = 'Comprador';
+					break;
 			}
-		}else{
-			echo "No sirvió";
+		} else {
+			header("location: ../../index.php");
 		}
-
-
-
-
-		/*********************************/
-		//metodos de mysqli
-
-		//query(coneccion,$query)
-		//num_rows($sql) 
-		//fecth_array($sql)
-		//fetch_row($sql)
+	} else {
+		echo "No sirvió";
 	}
- ?>
+
+
+
+
+	/*********************************/
+	//metodos de mysqli
+
+	//query(coneccion,$query)
+	//num_rows($sql) 
+	//fecth_array($sql)
+	//fetch_row($sql)
+}
--- php/log/select_finca.php
+++ php/log/select_finca.php
@@ -1,34 +1,35 @@
-<?php 
-	if (isset($_GET['finca'])) {
-		session_start();
-		if ($_GET['finca'] == "0") {
-			if ($_SESSION['id_rol'] != "2") {
-				header('location: ../../sitio/');				
-			}
+<?php
+if (isset($_GET['finca'])) {
+	session_start();
+	if ($_GET['finca'] == "0") {
+		if ($_SESSION['id_rol'] != "2") {
+			header('location: ../../sitio/');
 		}
-		$_SESSION['finca'] = $_GET['finca'];
-		//redireccionar a donde sea necesario
-		switch ($_SESSION['id_rol']) {
-			case "1":
-				header("location: ../../sitio/Admin");
-				$_SESSION['carpeta'] = 'Admin';
-				break;
-			case "2":
-				header("location: ../../sitio/Owner");
-				$_SESSION['carpeta'] = 'Owner' ;
-				break;
-			case "3":
-				header("location: ../../sitio/Comprador");
-				$_SESSION['carpeta'] = 'Comprador';
-				break;
-			case "4":
-				header("location: ../../sitio/Vendedor");
-				$_SESSION['carpeta'] = 'Vendedor';
-				break;
-			
-		}
-	}else{
-		header('location: ../../sitio/');
 	}
-
- ?>
+	$_SESSION['finca'] = $_GET['finca'];
+	//redireccionar a donde sea necesario
+	switch ($_SESSION['id_rol']) {
+		case "1":
+			header("location: ../../sitio/Admin");
+			$_SESSION['carpeta'] = 'Admin';
+			break;
+		case "2":
+			header("location: ../../sitio/Owner");
+			$_SESSION['carpeta'] = 'Owner';
+			break;
+		case "3":
+			header("location: ../../sitio/Comprador");
+			$_SESSION['carpeta'] = 'Comprador';
+			break;
+		case "4":
+			header("location: ../../sitio/Vendedor");
+			$_SESSION['carpeta'] = 'Vendedor';
+			break;
+		case "5":
+			$_SESSION['carpeta'] = 'Comprador';
+			header("location: ../../sitio/Comprador");
+			break;
+	}
+} else {
+	header('location: ../../sitio/');
+}
